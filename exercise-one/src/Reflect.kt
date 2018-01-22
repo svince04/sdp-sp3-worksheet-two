a72fea4cfc0edcd527e40f52dc4bd38dd0d2428a
essvee <s.vincent@nhm.ac.uk>
2018-01-22 22:29:17 +0000

    Completes Ex1

--- exercise-one/src/Reflect.kt
+++ exercise-one/src/Reflect.kt
@@ -1,8 +1,43 @@
+
 fun main(args: Array<String>) {
     if (args.size != 1) {
         println("Requires a class name as argument to program")
         System.exit(-1)
     }
 
-    // TODO
+    // Takes class name from input string and loads
+    val reflectClass = Class.forName(args[0])
+    val name = reflectClass.name
+
+    println("Class name: $name")
+    println("")
+
+    print("Interface name(s): ")
+    val interfaces = reflectClass.interfaces
+    interfaces.forEach { n -> println(n) }
+    println("")
+
+    println("Class fields:")
+    reflectClass.declaredFields.forEach { n -> println(n) }
+    println("")
+
+    println("Interface fields:")
+    interfaces.forEach { n -> n.declaredFields.forEach { i -> println(i) }}
+    println("")
+
+    println("Class constructor(s):")
+    reflectClass.constructors.forEach { n -> println(n) }
+    println("")
+
+    println("Interface constructor(s):")
+    interfaces.forEach { n -> n.constructors.forEach { i -> println(i) }}
+    println("")
+
+    println("Method(s):")
+    reflectClass.declaredMethods.forEach { n -> println(n)}
+    println("")
+
+    println("Interface method(s):")
+    interfaces.forEach { n -> n.declaredMethods.forEach { i -> println(i) }}
+    println("")
 }
